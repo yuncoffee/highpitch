54513ccb9a849a68a35f4a8144eaaf0d5fc0c246
leeyongjun604 <daniel_604@naver.com>
2023-11-13 21:47:48 +0900

    [Design] speedrate started

--- highpitch/Presentation/Overlay/SpeedPanelView.swift
+++ highpitch/Presentation/Overlay/SpeedPanelView.swift
@@ -15,11 +15,14 @@ struct SpeedPanelView: View {
             Text("\(SystemManager.shared.instantFeedbackManager.speechRecognizerManager?.realTimeRate ?? 300.0)")
                 .font(.title)
                 .foregroundColor(.black)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.white.opacity(0.2))
-                .edgesIgnoringSafeArea(.all)
-                .border(PanelData.shared.isEditMode ? (PanelData.shared.isFocused == 2 ? .purple : .white) : .clear, width: 2)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white.opacity(0.2))
+        .edgesIgnoringSafeArea(.all)
+        .border(PanelData.shared.isEditMode
+                ? (PanelData.shared.isFocused == 3
+                ? .purple : .white) : .clear, width: 2)
+        .clipShape(RoundedRectangle(cornerRadius: .HPCornerRadius.large))
         .onTapGesture {
             PanelData.shared.isFocused = 2
         }

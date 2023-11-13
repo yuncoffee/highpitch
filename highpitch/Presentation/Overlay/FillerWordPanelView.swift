a168a51d5a0781f1e27f9fd083db406c8a2e5820
leeyongjun604 <daniel_604@naver.com>
2023-11-13 21:32:18 +0900

    [Feat] filler word UI

--- highpitch/Presentation/Overlay/FillerWordPanelView.swift
+++ highpitch/Presentation/Overlay/FillerWordPanelView.swift
@@ -11,17 +11,53 @@ struct FillerwordPanelView: View {
     var floatingPanelController: FloatingPanelController
 
     var body: some View {
-        VStack {
-            Text("필러워드")
-                .font(.title)
-                .foregroundColor(.black)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.white.opacity(0.2))
-                .edgesIgnoringSafeArea(.all)
-                .border(PanelData.shared.isEditMode ? (PanelData.shared.isFocused == 3 ? .purple : .white) : .clear, width: 2)
+        let realTimeFillerCount = SystemManager.shared
+            .instantFeedbackManager.speechRecognizerManager?.realTimeFillerCount ?? 0
+        
+        VStack(spacing: .HPSpacing.xxxxsmall) {
+            ForEach(0..<3) { index in
+                FillerWordStatus(
+                    index: index,
+                    selected: index == 2
+                    ? realTimeFillerCount > 0 ? true : false
+                    : index == 1
+                    ? realTimeFillerCount > 1 ? true : false
+                    : realTimeFillerCount > 2 ? true : false
+                )
+            }
+            Text("습관어")
+                .systemFont(.caption)
+                .padding(.top, .HPSpacing.xxxxsmall)
+                .foregroundColor(Color("FFFFFF").opacity(0.6))
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black.opacity(0.4))
+        .edgesIgnoringSafeArea(.all)
+        .border(PanelData.shared.isEditMode
+                ? (PanelData.shared.isFocused == 3
+                ? .purple : .white) : .clear, width: 2)
         .onTapGesture {
             PanelData.shared.isFocused = 3
         }
+    }
+}
+
+struct FillerWordStatus: View {
+    var index: Int
+    var selected: Bool
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: .HPCornerRadius.small)
+            .fill(
+                selected
+                ? Color("8B6DFF").opacity(0.4)
+                : Color("D9D9D9").opacity(0.3)
+            )
+            .stroke(
+                selected
+                ? Color("FFFFFF").opacity(0.2)
+                : Color.clear
+            )
+            .frame(width: 44, height: 10)
     }
 }

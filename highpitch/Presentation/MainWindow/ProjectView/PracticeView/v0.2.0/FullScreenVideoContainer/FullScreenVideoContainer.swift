5db04b6fba12d2acfcc4d369bd99c2f5fa13b3f9
yuncoffee <dong072815@gmail.com>
2023-11-13 22:55:33 +0900

    [Design] 전체화면 레이아웃 조정 #8

--- highpitch/Presentation/MainWindow/ProjectView/PracticeView/v0.2.0/FullScreenVideoContainer/FullScreenVideoContainer.swift
+++ highpitch/Presentation/MainWindow/ProjectView/PracticeView/v0.2.0/FullScreenVideoContainer/FullScreenVideoContainer.swift
@@ -28,62 +28,29 @@ struct FullScreenVideoContainer: View {
     private let PRACTICE_HEADER_INFO_HEIGHT = 168.0
     private let VIDEO_CONTROLLER_HEIGHT = 64.0
     private let VERTICAL_PADDING = 48.0
+    private let FOOTER_HOVER_CONTAINER_HEIGHT = 54.0
     
     var body: some View {
         GeometryReader(content: { geometry in
             let maxWidth = geometry.size.width - SCRIPT_CONTAINER_WIDTH
             let maxHeight = geometry.size.height - INDICATOR_HEIGHT - VIDEO_CONTROLLER_HEIGHT - PRACTICE_HEADER_INFO_HEIGHT - VERTICAL_PADDING
-            
             ZStack(alignment: .topLeading) {
                 /// video
                 VStack {
-//                    Text("Video")
+                    //                    Text("Video")
                 }
-                .frame(
-                    maxWidth: .infinity,
-                    maxHeight: .infinity
-                )
+                .frame(maxWidth: .infinity, maxHeight: .infinity )
                 .background(viewStore.isFullScreenTransition ? Color.brown : .clear)
                 .offset(
                     x: 0,
                     y: viewStore.isFullScreenTransition ? 0 : PRACTICE_HEADER_INFO_HEIGHT
                 )
-                VStack {
-                    Text("Title")
-                    Text("subTitle")
-                }
-                .frame(maxWidth: .infinity, maxHeight: VIDEO_CONTROLLER_HEIGHT, alignment: .topLeading)
-                .background(Color.purple)
-                .offset(y: viewStore.isFullScreenTransition && isFullScreenVideoHover
-                        ? .zero
-                        : -VIDEO_CONTROLLER_HEIGHT
-                )
-                .opacity(viewStore.isFullScreenTransition ? 1 : 0)
+                headerHoverContainer
+                footerHoverContainer(geoHeight: geometry.size.height)
                 /// footer
-                HStack {
-                    Text("토글")
-                    Text("토글토글")
-                    Text("토글토글토글")
-                    Text("전체화면")
-                        .onTapGesture {
-                            withAnimation {
-                                viewStore.isFullScreenVideoActive = false
-                            }
-                        }
-                }
-                .frame(maxWidth: .infinity, maxHeight: 56)
-                .background(Color.blue)
-                .offset(y: viewStore.isFullScreenTransition && isFullScreenVideoHover
-                        ? viewStore.currentFeedbackViewType == .every
-                        ? geometry.size.height - VIDEO_CONTROLLER_HEIGHT - VERTICAL_PADDING
-                        : geometry.size.height - 150
-                        : geometry.size.height
-                )
-                .opacity(viewStore.isFullScreenTransition ? 1 : 0)
-                .onTapGesture {
-                    print("HELL?O?")
-                }
+                
             }
+            .padding(.bottom, VIDEO_CONTROLLER_HEIGHT)
             .frame(
                 maxWidth: viewStore.isFullScreenTransition ? .infinity : maxWidth,
                 maxHeight: viewStore.isFullScreenTransition ? .infinity : maxHeight
@@ -95,6 +62,7 @@ struct FullScreenVideoContainer: View {
             }
         })
         .zIndex(viewStore.isFullScreenVideoVisible ? 1 : 0)
+        .opacity(viewStore.isFullScreenVideoVisible ? 1 : 0)
         .onAppear {
             // MARK: - Add MockData
 #if PREVIEW
@@ -105,9 +73,87 @@ struct FullScreenVideoContainer: View {
             if let url = url {
                 viewStore.practice.audioPath = url
             }
-            viewStore.isFullScreenVideoActive = true
+//            viewStore.isFullScreenTransition = true
+//            isFullScreenVideoHover = true
 #endif
         }
+    }
+}
+
+extension FullScreenVideoContainer {
+    @ViewBuilder
+    var headerHoverContainer: some View {
+        VStack(alignment: .leading, spacing: .zero) {
+            Text("\(viewStore.practice.practiceName)")
+                .systemFont(.largeTitle)
+                .foregroundStyle(Color.HPGray.systemWhite)
+            Text("\(Date.createAtToYearMonthDayWithTime(input: viewStore.practice.creatAt))")
+                .systemFont(.caption)
+                .foregroundStyle(Color.HPGray.systemWhite.opacity(0.6))
+            
+        }
+        .padding(.top, .HPSpacing.xxsmall)
+        .padding(.horizontal, .HPSpacing.medium)
+        .frame(maxWidth: .infinity, maxHeight: VIDEO_CONTROLLER_HEIGHT, alignment: .topLeading)
+        .background(
+            LinearGradient(
+                gradient: Gradient(
+                    colors: [
+                        .HPGray.systemBlack.opacity(0.6),
+                        .HPGray.systemBlack.opacity(0)
+                    ]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+        )
+        .offset(y: viewStore.isFullScreenTransition && isFullScreenVideoHover
+                ? .zero
+                : -VIDEO_CONTROLLER_HEIGHT
+        )
+        .opacity(viewStore.isFullScreenTransition && isFullScreenVideoHover ? 1 : 0)
+    }
+    
+    @ViewBuilder
+    func footerHoverContainer(geoHeight: CGFloat) -> some View {
+        HStack(alignment: .top) {
+            HStack {
+                Text("토글")
+                Text("토글토글")
+                Text("토글토글토글")
+                Text("전체화면")
+                    .onTapGesture {
+                        withAnimation {
+                            viewStore.isFullScreenVideoActive = false
+                        }
+                    }
+            }
+            .frame(maxWidth: .infinity, maxHeight: FOOTER_HOVER_CONTAINER_HEIGHT)
+            .border(.blue)
+        }
+        .frame(
+            maxWidth: .infinity,
+            maxHeight: FOOTER_HOVER_CONTAINER_HEIGHT + INDICATOR_HEIGHT,
+            alignment: .top
+        )
+        .background(
+            LinearGradient(
+                gradient: Gradient(
+                    colors: [
+                        .HPGray.systemBlack.opacity(0.6),
+                        .HPGray.systemBlack.opacity(0)
+                    ]),
+                startPoint: .bottom,
+                endPoint: .top
+            )
+        )
+        .border(.red)
+        .offset(y: viewStore.isFullScreenTransition && isFullScreenVideoHover
+                ? viewStore.currentFeedbackViewType == .every
+                ? geoHeight - VIDEO_CONTROLLER_HEIGHT - VERTICAL_PADDING
+                : geoHeight - VIDEO_CONTROLLER_HEIGHT - INDICATOR_HEIGHT - FOOTER_HOVER_CONTAINER_HEIGHT
+                : geoHeight
+        )
+        .opacity(viewStore.isFullScreenTransition && isFullScreenVideoHover ? 1 : 0)
     }
 }
 

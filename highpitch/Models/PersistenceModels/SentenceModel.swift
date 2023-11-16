77b92085d2cec81ea36975f4a0a6ded09c8849a0
ljh <dusk5588@naver.com>
2023-11-16 18:15:41 +0900

    [Feat] Schema 및 MigrationPlan 구현 (SwiftData관련 오류발생시 여기서부터 재수정

--- highpitch/Models/PersistenceModels/SentenceModel.swift
+++ highpitch/Models/PersistenceModels/SentenceModel.swift
@@ -15,6 +15,8 @@ class SentenceModel {
     var startAt: Int
     var endAt: Int
     var epmValue: Double
+    var spmValue: Double = -1.0
+    var fastOrSlow: Int = 0
     
     init(index: Int, sentence: String, startAt: Int = -1, endAt: Int = -1, epmValue: Double = -1.0) {
         self.index = index

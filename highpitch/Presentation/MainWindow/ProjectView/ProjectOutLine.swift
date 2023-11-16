c7ef730ccfbb2bd3b3c746d034b48d9e51354244
leeyongjun604 <daniel_604@naver.com>
2023-11-16 14:50:07 +0900

    [Feat] ProjectOutLineView in progress

--- highpitch/Presentation/MainWindow/ProjectView/ProjectOutLine.swift
+++ highpitch/Presentation/MainWindow/ProjectView/ProjectOutLine.swift
@@ -11,12 +11,6 @@ import Charts
 struct ProjectOutLine: View {
     @Environment(ProjectManager.self)
     private var projectManager
-    @State
-    var speakingRate = 0
-    @State
-    var fillerWordPerMinute = 0.0
-    @State
-    var isLevelTooltipActive = false
     
     var body: some View {
         if let practices = projectManager.current?.practices.sorted(by: { $0.creatAt < $1.creatAt }) {
@@ -42,7 +36,7 @@ struct ProjectOutLine: View {
                                 speakingRateView(practices: practices)
                                 fillerWordCountView(practices: practices)
                             }
-                            speakingRateView(practices: practices)
+                            fillerWordTOP3View(practices: practices)
                         }
                         .padding(.bottom, .HPSpacing.xxsmall)
                         /// [레벨, 습관어, 말 빠르기] 그래프
@@ -51,6 +45,37 @@ struct ProjectOutLine: View {
                 }.border(.red)
             } else { emptyView }
         } else { emptyView }
+    }
+}
+
+extension ProjectOutLine {
+    // MARK: - functions
+    /// speakingRage(SPM)을 반환합니다.
+    func returnSpeakingRate(practices: [PracticeModel]) -> Int {
+        var answer = 0.0
+        for practice in practices {
+            answer += practice.summary.epmAverage
+        }
+        answer /= Double(practices.count)
+        return Int(answer)
+    }
+    
+    /// fillerWordTOP3를 반환합니다.
+    func returnfillerWordTOP3(practices: [PracticeModel]) -> Array<(key: String, value: Int)> {
+        var answer: Dictionary<String, Int> = Dictionary<String, Int>()
+        for practice in practices {
+            for fillerWord in practice.summary.eachFillerWordCount {
+                if answer[fillerWord.fillerWord] == nil {
+                    answer[fillerWord.fillerWord] = fillerWord.count
+                } else {
+                    answer[fillerWord.fillerWord] = answer[fillerWord.fillerWord]! + fillerWord.count
+                }
+            }
+        }
+        var sortedAnswer = answer.sorted{$0.1 > $1.1}
+        while !sortedAnswer.isEmpty && sortedAnswer.last!.value == 0 { _ = sortedAnswer.popLast() }
+        while sortedAnswer.count > 3 { _ = sortedAnswer.popLast() }
+        return sortedAnswer
     }
 }
 
@@ -65,13 +90,72 @@ extension ProjectOutLine {
     
     // MARK: - speakingRateView
     func speakingRateView(practices: [PracticeModel]) -> some View {
-        VStack(spacing: 0) {
-            Text("습관어")
+        var speakingRate = returnSpeakingRate(practices: practices)
+        return VStack(spacing: 0) {
+            Text("말 빠르기")
                 .systemFont(.body)
                 .foregroundColor(Color.HPTextStyle.darker)
-            Text("\(speakingRate)회")
+            Text("\(speakingRate)SPM")
                 .systemFont(.largeTitle)
                 .foregroundColor(Color.HPPrimary.base)
+            Text(speakingRate >= 410
+                 ? "빠른 편이에요"
+                 : speakingRate >= 370
+                 ? "조금 빠른 편이에요"
+                 : speakingRate >= 330
+                 ? "적절한 빠르기에요"
+                 : speakingRate >= 290
+                 ? "조금 느린 편이에요"
+                 : "느린 편이에요"
+            )
+                .systemFont(.caption)
+                .foregroundColor(Color.HPTextStyle.light)
+        }
+        .frame(minWidth: 50, maxWidth: .infinity, minHeight: 120, maxHeight: 120, alignment: .center)
+        .background(Color.HPComponent.Section.background)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .shadow(color: Color.HPComponent.shadowColor ,radius: 10, y: .HPSpacing.xxxxsmall)
+    }
+    // MARK: - fillerWordCountView
+    func fillerWordCountView(practices: [PracticeModel]) -> some View {
+        VStack(spacing: 0) {
+            Text("습관어 사용")
+                .systemFont(.body)
+                .foregroundColor(Color.HPTextStyle.darker)
+            HStack(alignment: .bottom, spacing: .HPSpacing.xxxxsmall) {
+                Text("분당")
+                    .systemFont(.caption2)
+                    .foregroundColor(Color.HPTextStyle.light)
+                    .offset(y: -7)
+                Text("3.2회")
+                    .systemFont(.largeTitle)
+                    .foregroundColor(Color.HPPrimary.base)
+            }
+            Text("적절한 편이에요")
+                .systemFont(.caption)
+                .foregroundColor(Color.HPTextStyle.light)
+        }
+        .frame(minWidth: 50, maxWidth: .infinity, minHeight: 120, maxHeight: 120, alignment: .center)
+        .background(Color.HPComponent.Section.background)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .shadow(color: Color.HPComponent.shadowColor ,radius: 10, y: .HPSpacing.xxxxsmall)
+    }
+    
+    // MARK: - fillerWordTOP3View
+    func fillerWordTOP3View(practices: [PracticeModel]) -> some View {
+        VStack(spacing: 0) {
+            Text("많이 사용한 습관어")
+                .systemFont(.body)
+                .foregroundColor(Color.HPTextStyle.darker)
+            HStack(alignment: .bottom, spacing: .HPSpacing.xxxxsmall) {
+                Text("분당")
+                    .systemFont(.caption2)
+                    .foregroundColor(Color.HPTextStyle.light)
+                    .offset(y: -7)
+                Text("3.2회")
+                    .systemFont(.largeTitle)
+                    .foregroundColor(Color.HPPrimary.base)
+            }
             Text("적절한 말 빠르기에요")
                 .systemFont(.caption)
                 .foregroundColor(Color.HPTextStyle.light)
@@ -80,50 +164,5 @@ extension ProjectOutLine {
         .background(Color.HPComponent.Section.background)
         .clipShape(RoundedRectangle(cornerRadius: 12))
         .shadow(color: Color.HPComponent.shadowColor ,radius: 10, y: .HPSpacing.xxxxsmall)
-        .onAppear {
-            speakingRate = returnSpeakingRate(practices: practices)
-        }
-    }
-    // MARK: - fillerWordCountView
-    func fillerWordCountView(practices: [PracticeModel]) -> some View {
-        VStack(spacing: 0) {
-            Text("말 빠르기")
-                .systemFont(.body)
-                .foregroundColor(Color.HPTextStyle.darker)
-            Text("\(speakingRate)SPM")
-                .systemFont(.largeTitle)
-                .foregroundColor(Color.HPPrimary.base)
-            Text("적절한 말 빠르기에요")
-                .systemFont(.caption)
-                .foregroundColor(Color.HPTextStyle.light)
-        }
-        .border(.blue)
-        .padding(.horizontal, .HPSpacing.small)
-        .frame(minHeight: 120, maxHeight: 120, alignment: .leading)
-        .background(Color.HPComponent.Section.background)
-        .clipShape(RoundedRectangle(cornerRadius: 12))
-        .shadow(color: Color.HPComponent.shadowColor ,radius: 10, y: .HPSpacing.xxxxsmall)
-        .onAppear {
-            speakingRate = returnSpeakingRate(practices: practices)
-            fillerWordPerMinute = fillerWordPerMinute(practices: practices)
-        }
-    }
-    //MARK: - functions
-    /// speakingRage(SPM)을 반환합니다.
-    func returnSpeakingRate(practices: [PracticeModel]) -> Int {
-        var answer = 0.0
-        for practice in practices {
-            answer += practice.summary.epmAverage
-        }
-        answer /= Double(practices.count)
-        return Int(answer)
-    }
-    
-    /// 분당 습관어 사용횟수를 반환합니다.
-    func fillerWordPerMinute(practices: [PracticeModel]) -> Double {
-        var answer = 0.0
-        for practice in practices {
-        }
-        return answer
     }
 }

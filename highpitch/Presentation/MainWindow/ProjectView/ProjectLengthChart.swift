a98c3393bdd19006fc10003744078666d70a352a
leeyongjun604 <daniel_604@naver.com>
2023-11-18 23:37:22 +0900

    [Feat] done with ProjectFWPMChart

---
+++ highpitch/Presentation/MainWindow/ProjectView/ProjectLengthChart.swift
@@ -0,0 +1,18 @@
+//
+//  ProjectLengthChart.swift
+//  highpitch
+//
+//  Created by 이용준의 Macbook on 11/18/23.
+//
+
+import SwiftUI
+
+struct ProjectLengthChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProjectLengthChart()
+}
